0598ca91a66f808c0d86190b39c3f3de9c5d177c
Carlo Duan <carlo.duan@achievers.com>
2021-04-18 15:30:19 -0400

    utiized producer and consumer in both django and flask app

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,4 +1,6 @@
-import pika, os
+import pika
+import os
+import json
 
 url = os.environ.get('CLOUDAMQP_URL',
                         'amqps://oohvqrza:qabN5LAF9yQyv_9bwaMh-VXRzMts5Dpe@jaguar.rmq.cloudamqp.com/oohvqrza')
@@ -6,11 +8,9 @@ url = os.environ.get('CLOUDAMQP_URL',
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
-channel.queue_declare(queue='admin')
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello CloudAMQP!')
 
-print(" [x] Sent 'Hello World!'")
-connection.close()
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
 
--- main/consumer.py
+++ main/consumer.py
@@ -1,2 +1,39 @@
-import pika, os
+import pika
+import os
+import json
+from app import Product, db
 
+url = os.environ.get('CLOUDAMQP_URL', 'amqps://oohvqrza:qabN5LAF9yQyv_9bwaMh-VXRzMts5Dpe@jaguar.rmq.cloudamqp.com/oohvqrza')
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='main')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received from main")
+    data = json.loads(body)
+    print(data)
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        print(product)
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+
+
+channel.basic_consume(queue='main',
+                      on_message_callback=callback,
+                      auto_ack=True)
+
+print(' [*] Waiting for messages:')
+channel.start_consuming()
+connection.close()
