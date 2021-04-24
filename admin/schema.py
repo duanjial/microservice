31bb5189f718747cc2ceff28740705828cfce5c9
duanjial <carlo.duan@gmail.com>
2021-04-23 22:13:26 -0400

    added get product by title function

--- admin/schema.py
+++ admin/schema.py
@@ -12,9 +12,16 @@ class ProductType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_products = graphene.List(ProductType)
+    product_by_title = graphene.Field(ProductType, title=graphene.String(required=True))
 
     def resolve_all_products(self, root):
         return Product.objects.all()
+
+    def resolve_product_by_title(self, root, title):
+        try:
+            return Product.objects.get(title=title)
+        except Product.DoesNotExist:
+            return None
 
 
 schema = graphene.Schema(query=Query)
