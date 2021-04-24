b284e8770fc2024c4047a0217edfd3c961fb2df2
duanjial <carlo.duan@gmail.com>
2021-04-23 22:30:58 -0400

    changed to static method

--- admin/schema.py
+++ admin/schema.py
@@ -7,21 +7,27 @@ from products.models import Product
 class ProductType(DjangoObjectType):
     class Meta:
         model = Product
-        fields = ("id", "title", "image", "likes")
+        fields = ("title", "image", "likes")
 
 
 class Query(graphene.ObjectType):
     all_products = graphene.List(ProductType)
     product_by_title = graphene.Field(ProductType, title=graphene.String(required=True))
 
+    @staticmethod
     def resolve_all_products(self, root):
         return Product.objects.all()
 
+    @staticmethod
     def resolve_product_by_title(self, root, title):
         try:
             return Product.objects.get(title=title)
         except Product.DoesNotExist:
             return None
+
+
+class Mutation(graphene.ObjectType):
+    pass
 
 
 schema = graphene.Schema(query=Query)
